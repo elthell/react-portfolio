1cd7df797fdc8b899284f428c0cb4e76c8ac7aa6
elthell <eleanor.thell@gmail.com>
2023-11-05 20:09:17 -0600

    add about page + app / index css

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <section
+      id="about"
+      class="d-flex justify-content-center align-items-center m-3"
+    >
+      <div class="section-text p-3 shadow">
+        <p class="section-title fs-3 fw-bold text-center">About</p>
+        <div class="row">
+          <p class="fs-6">
+            Hi, I'm Eleanor!
+            <br />
+            Data-driven, life-long learner looking to bring a History B.A.'s
+            love of documentation and presenting information to a career shift
+            into tech.
+            <br />• skilled in customer service and data entry with growing
+            skills in full stack web development.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

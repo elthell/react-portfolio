b055147839091b43909224d7198a7fde669874f9
elthell <eleanor.thell@gmail.com>
2023-11-06 11:08:39 -0600

    add portfolioData + feed in to project list

---
+++ src/assets/portfolioData.js
@@ -0,0 +1,68 @@
+const projects = [
+  {
+    id: 1,
+    title: "Dad-icated",
+    description:
+      "A dad joke ranking application where users can create a profile to see dad jokes, as well as vote on them and submit their own.",
+    stack: "Node.js • MySQL • Handlebars.js",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/Dtrieu1/DAD-ICATED",
+    liveLink: "https://dad-icated-6a2b4854840e.herokuapp.com/",
+  },
+  {
+    id: 2,
+    title: "Food Finder",
+    description:
+      "Utilizing the Spoonacular API, the application allows users to search and save recipes, as well as find local food of the same cuisine as the recipes they've saved",
+    stack: "Javascript • HTML • Bulma • Spoonacular API",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/Thinker92/FoodFinder",
+    liveLink: "https://thinker92.github.io/FoodFinder/",
+  },
+  {
+    id: 3,
+    title: "Portfolio v.1",
+    description:
+      "A previous version of a portfolio to showcase my skills and alents to employers through a detailed about section, interactive project display, and contact links",
+    stack: "HTML • CSS",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/elthell/portfolio",
+    liveLink: "https://elthell.github.io/portfolio/",
+  },
+  {
+    id: 4,
+    title: "Timed Quiz",
+    description:
+      "This application tests a user's knowledge with five questions about Warhammer 40k to answer over 45 seconds. Wrong answers deduct time and at the end users can save their score.",
+    stack: "Javascript • HTML • Bootstrap",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/elthell/timed-quiz",
+    liveLink: "https://elthell.github.io/timed-quiz/",
+  },
+  {
+    id: 5,
+    title: "Social Network API",
+    description: "This application is an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
+    stack: "Express.js • MongoDB • Mongoose",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/elthell/social-api",
+    liveLink: "https://drive.google.com/file/d/1fKQrxbLSiR0n9WjTMFU7KvE4WsTRP9QE/view",
+  },
+  {
+    id: 6,
+    title: "Note Take",
+    description: "This application can be used to write and save notes, using an Express.js back end and will save and retrieve note data from a JSON file",
+    stack: "Express.js • JSON",
+    img: "",
+    imgAlt: "",
+    codeLink: "https://github.com/elthell/note-taker",
+    liveLink: "https://note-taker-20664-0064acf3cbe5.herokuapp.com/",
+  },
+];
+
+export { projects }
--- src/components/pages/ProjectList.jsx
+++ src/components/pages/ProjectList.jsx
@@ -1,15 +1,16 @@
-import Project from "../cards/Project";
+import ProjectCard from "../cards/Project";
+import { projects } from "../../assets/portfolioData";
 
-const ProjectList = ({ projects }) => {
+const ProjectList = () => {
   const renderedList = projects.map(({ id, ...project }) => {
-    return <Project key={id} project={project} />;
+    return <ProjectCard key={id} project={project} />;
   });
 
   return (
     <section id="projects" class="mx-auto">
       <p class="section-title fs-3 fw-bold text-center py-2 my-2">
         <i class="flower fa-solid fa-fan"></i>
-        Work
+        Projects
       </p>
       <div class="card-row row">{renderedList}</div>
     </section>
